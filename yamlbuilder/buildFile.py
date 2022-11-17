b8d377110956fb6da38272031c7d8a28f2bd5b90
GoncaloMark <gmgoncalo7@gmail.com>
2022-11-17 22:08:46 +0000

    Added support for attribute values and worked a bit towards cleaning up code that wasn't necessary!

--- yamlbuilder/buildFile.py
+++ yamlbuilder/buildFile.py
@@ -5,8 +5,7 @@ except ImportError as e:
     print("Some modules could not be imported from stdlib('yaml')")
 
 class GetInput:
-    def __init__(self, type):
-        self.__type = type
+    def __init__(self):
         self._url = None
         self.__hops = None
         self.__tags= None
@@ -14,12 +13,9 @@ class GetInput:
         self.__selecs = None
         self.__ID = None
         self._yaml_dict = None
+        self.__attrsv = None
 
-        if self.__type == "cli":
-            self._input = self.__getInputCLI()
-
-        elif self.__type == "web":
-            self._input = self.__getInputWeb()
+        self.__getInputCLI()
 
     def __getInputCLI(self):
         print("Config File Setup! What's the URL?")
@@ -32,38 +28,27 @@ class GetInput:
         self.__tags = list(input().strip().split())
         print("Any attributes?")
         self.__attrs = list(input().strip().split())
+        print("Attribute(s) value(s)?")
+        self.__attrsv = list(input().strip().split())
         print("Selectors?")
         self.__selecs = list(input().strip().split())
-        if "id" in self.__selecs:
+        if "id" in self.__selecs or "id" in self.__attrs:
             print("ID value(s)?")
             self.__ID = list(input().strip().split())
         print("Classes?")
         self.__classes = list(input().strip().split())
-
-        if self.__ID == None:
-            self._yaml_dict = self.__buildDict(self._url, self.__hops, self.__tags, self.__attrs, self.__selecs, self.__classes)
-        else:
-            self._yaml_dict = self.__buildDict(self._url, self.__hops, self.__tags, self.__attrs, self.__selecs, self.__ID, self.__classes)
+        self._yaml_dict = self.__buildDict(self._url, self.__hops, self.__tags, self.__attrs, self.__attrsv, self.__selecs,self.__ID, self.__classes)
 
     def __buildDict(self, *args):
-        if self.__ID == None:
-            dict = {
+        dict = {
                 'url': args[0],
                 'hops': args[1],
                 'tags': args[2], 
                 'attrs': args[3],
-                'selectors': args[4],
-                'classes': args[5]
-            }
-        else:
-            dict = {
-                'url': args[0],
-                'hops': args[1],
-                'tags': args[2], 
-                'attrs': args[3],
-                'selectors': args[4],
-                'IDv': args[5],
-                'classes': args[6]
+                'attrV':args[4],
+                'selectors': args[5],
+                'IDv': args[6],
+                'classes': args[7]
             }
         
         return dict
@@ -73,8 +58,8 @@ class GetInput:
 
 
 class BuildFile(GetInput):
-    def __init__(self, type):
-        super().__init__(type=type)
+    def __init__(self):
+        super().__init__()
         self.__createFile()
 
     def __createFile(self):
@@ -83,5 +68,5 @@ class BuildFile(GetInput):
                 yaml.dump(self._yaml_dict, file)
 
 if __name__ == "__main__":
-    x = BuildFile(type="cli") #this is a test!
+    x = BuildFile() #this is a test!
     pass
